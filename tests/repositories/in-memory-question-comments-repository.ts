409272149bc6861de5cb30c8cd22eb321fc342de
WalberCesar <walber.santos01@etec.sp.gov.br.com>
2024-09-08 21:47:26 -0300

    create comment on question use case, repository and test

---
+++ tests/repositories/in-memory-question-comments-repository.ts
@@ -0,0 +1,12 @@
+import { QuestionCommentsRepository } from '@/domain/forum/application/repositories/question-comments-repoitory'
+import { QuestionComment } from '@/domain/forum/enterprise/entities/question-comment'
+
+export class InMemoryQuestionCommentsRepository
+  implements QuestionCommentsRepository
+{
+  public items: QuestionComment[] = []
+
+  async create(questionComment: QuestionComment) {
+    this.items.push(questionComment)
+  }
+}
